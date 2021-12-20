fbee24a27e861d7e80de424b51810bf1da24db6f
tokuyama <tokuyama.raido@vbest.jp>
2021-12-21 08:27:05 +0900

    Test for vercel automatically deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Layout from 'components/Layout';
 const Home = () => {
   return (
     <Layout title='Home'>
-      <p className='text-4xl'>Welcome to Holo Goroku !</p>
+      <p className='text-4xl'>Welcome to Holo Goroku 2!</p>
     </Layout>
   );
 };
